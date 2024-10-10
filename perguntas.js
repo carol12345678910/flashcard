ead34896672d81e784ceeb8388bac6d0404c075a
carol12345678910 <161325319+carol12345678910@users.noreply.github.com>
2024-10-10 07:55:01 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,35 +1,35 @@
 criaCartao(
-    'Arte'
-    'Quem pintou a Monalisa?'
+    'Arte',
+    'Quem pintou a Monalisa?',
     'Leonardo da Vinci'
 )
 
 criaCartao(
-    'Arte'
-    'Quem pintou a Monalisa?'
+    'Arte',
+    'Quem pintou a Monalisa?',
     'Leonardo da Vinci'
 )
 
 criaCartao(
-    'Arte'
-    'Quem pintou a Monalisa?'
+    'Arte',
+    'Quem pintou a Monalisa?',
     'Leonardo da Vinci'
 )
 
 criaCartao(
-    'Arte'
-    'Quem pintou a Monalisa?'
+    'Arte',
+    'Quem pintou a Monalisa?',
     'Leonardo da Vinci'
 )
 
 criaCartao(
-    'Arte'
-    'Quem pintou a Monalisa?'
+    'Arte',
+    'Quem pintou a Monalisa?',
     'Leonardo da Vinci'
 )
 
 criaCartao(
-    'Arte'
-    'Quem pintou a Monalisa?'
+    'Arte',
+    'Quem pintou a Monalisa?',
     'Leonardo da Vinci'
 )
